9ab350b8d0e75d8fd6b67e18389fad3971c7f794
Hyung Kim <hjkmines@gmail.com>
2020-07-03 20:06:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,8 +10,26 @@ class Pokemon
   
   def save 
     sql = <<-SQL 
-      INSERT INTO pokemons 
-      VALUES 
+      INSERT INTO pokemons (name, type)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.type)
+  end 
   
+  def self.new_on_db(row)
+    id = row[0]
+    name = row[1]
+    type = row[2]
+    pokemon = Pokemon.new(id, name, type)
+  end 
+  
+  def self.find(id) 
+  sql = <<-SQL   
+    SELECT * 
+    FROM pokemons 
+    WHERE id = ?
+  SQL
+  DB[:conn].execute(sql, id)
+  end 
   
 end
